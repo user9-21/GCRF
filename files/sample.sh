609317266f8d5781f25bc597cf697c6cee56e917
user9-21 <92918900+user9-21@users.noreply.github.com>
2022-06-24 11:59:03 +0530

    Update sample.sh

--- files/sample.sh
+++ files/sample.sh
@@ -1,11 +1,30 @@
-curl -o default.sh https://raw.githubusercontent.com/user9-21/GoogleCloudReady-Facilitator-program/main/files/default.sh
+curl -o default.sh https://raw.githubusercontent.com/user9-21/GCRF/main/files/default.sh
 source default.sh
 
 
 
 completed "Task 1"
 
+
 completed "Task 2"
+
+
+completed "Task 3"
+
+
+completed "Task 4"
+
+
+completed "Task 5"
+
+
+completed "Task 6"
+
+
+completed "Task 7"
+
+
+completed "Task 8"
 
 completed "Lab"
 
